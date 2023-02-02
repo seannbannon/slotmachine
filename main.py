bee6b3c11904761352e25dcd42054b14e453498e
Sean Bannon <seannbannon@gmail.com>
2023-02-01 20:38:56 -0600

    create file and deposit function

---
+++ main.py
@@ -0,0 +1,11 @@
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0!")
+        else:
+            print("Please enter a number.")
