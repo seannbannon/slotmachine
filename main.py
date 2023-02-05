df5d4384de031a129dc0bf6623a0c06e522c37a3
Sean Bannon <seannbannon@gmail.com>
2023-02-04 21:20:32 -0600

    making a while loop that makes sure total bet does not exceed balance

--- main.py
+++ main.py
@@ -47,9 +47,16 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
-    print(f"You are betting ${bet} on {lines} lines. Your total bet is ${total_bet}. Good luck.")
+
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount. Your current balance is ${balance}.")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Your total bet is equal to: ${total_bet}. Good luck.")
 
 
 main()
