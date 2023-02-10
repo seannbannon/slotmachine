92400a703d491fe3ed8ca577a53dd18ea204df54
Sean Bannon <seannbannon@gmail.com>
2023-02-10 07:46:45 -0600

    seems to be working

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def get_bet():
 
     return bet
 
-def spin():
+def spin(balance):
     lines = get_number_of_lines()
 
     while True:
@@ -139,16 +139,18 @@ def spin():
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won {winnings}")
     print(f"You won on lines:", *winning_lines)
-    return winning_lines - total_bet
+    return winnings - total_bet
 
 
 def main():
     balance = deposit()
     while True:
         print(f"Current balance is: ${balance}")
-        spin = input("Press enter to spin (q to quit).")
-        if spin == "q":
+        answer = input("Press enter to play (q to quit).")
+        if answer == "q":
             break
-        balance += spin()
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
 
 main()
